770cba9f1627998777c09a958f529fe7a718a73c
Pornphawit Manasut <wit@dyn-vpn-154-029.abo.fi>
2023-02-02 21:15:32 +0200

    make that YOLOv3 object has classes attribute as class attribute so that factory function can be ubiquitous and just return YOLOv3 instead of specific

--- models/yolov3.py
+++ models/yolov3.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 class YOLOv3(object):
+    classes = []  # classes attribute will get override by inherited class if it's defined
     anchors = np.asarray(
         [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45], [59, 119], [116, 90], [156, 198], [373, 326]])
 
